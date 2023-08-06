e81c29b27c0b04d7888cba819a59b67ace46ae28
binudrinker <56517440+fomosort@users.noreply.github.com>
2023-08-05 19:53:32 -0700

    Fix update display to show the entire calculation
    updateDisplay now correctly parses calcArray, converting function to symbols

--- script.js
+++ script.js
@@ -15,64 +15,6 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-
-//Variables for Operation & Numbers
-let num1;
-let operation;
-let num2;
-let operationDone = false;
-function execOperation(num1, num2, operation) {
-  num1 = parseFloat(num1);
-  num2 = parseFloat(num2);
-  console.log(operation(num1, num2));
-  isNum2 = false;
-  operationDone = true;
-  return operation(num1, num2);
-}
-
-function updateDisplay(num1String, operation, num2String) {
-  document.querySelector("#number1").textContent = num1String;
-  let operatorSymbol;
-
-  if (operation) {
-    switch (operation.name) {
-      case "add":
-        operatorSymbol = "+";
-        break;
-      case "subtract":
-        operatorSymbol = "-";
-        break;
-      case "multiply":
-        operatorSymbol = "x";
-        break;
-      case "divide":
-        operatorSymbol = "➗";
-        break;
-    }
-
-    document.querySelector("#operator").textContent = operatorSymbol;
-  }
-
-  document.querySelector("#number2").textContent = num2String;
-}
-
-function clearDisplay() {
-  num1String = "";
-  num2String = "";
-  operation = null;
-  document.querySelector("#number1").textContent = num1String;
-  document.querySelector("#operator").textContent = "";
-  document.querySelector("#number2").textContent = num2String;
-}
-document.querySelector("button#clear").addEventListener("click", clearDisplay);
-
-//TODO: Track place in array
-//Rules for array: last place in array must be a number
-//array[0] is number
-//array[1, 3, 5, 7, 9] must be operators
-
-//last place in array = array.length -1
-
 //tracks all numbers and operations in an array
 const calcArray = [];
 let currentPos;
@@ -95,7 +37,7 @@ function trackOperations(buttonInput) {
   } else {
     calcArray.push(buttonInput);
     currentPos = calcArray.length();
-    return
+    return;
   }
 
   if (inputShouldBe === "operator") {
@@ -106,9 +48,72 @@ function trackOperations(buttonInput) {
   } else {
     calcArray.push(buttonInput);
     currentPos = calcArray.length();
-    return
+    return;
   }
 }
+
+//Variables for Operation & Numbers
+let num1;
+let operation;
+let num2;
+let operationDone = false;
+function execOperation(num1, num2, operation) {
+  //FIXME:
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  console.log(operation(num1, num2));
+  isNum2 = false;
+  operationDone = true;
+  return operation(num1, num2);
+}
+
+/** @param {Array} calcArray */
+function updateDisplay(calcArray) {
+  let operatorSymbol;
+  //swap all the functions to displayable symbols
+  const calcArraySymbols = calcArray.map((val, index, array) => {
+    if (index !== 0 && index % 2 !== 0) {
+      switch (val.name) {
+        case "add":
+          operatorSymbol = "+";
+          break;
+        case "subtract":
+          operatorSymbol = "-";
+          break;
+        case "multiply":
+          operatorSymbol = "x";
+          break;
+        case "divide":
+          operatorSymbol = "➗";
+          break;
+      }
+      return operatorSymbol
+    }
+    return val
+  })
+  console.log(calcArraySymbols);
+  ;
+  const calcArrayString = calcArraySymbols.join(' ')
+  document.querySelector('.display#main').textContent = calcArrayString
+}
+updateDisplay([1,add,2,add,3,multiply])
+
+function clearDisplay() {
+  num1String = "";
+  num2String = "";
+  operation = null;
+  document.querySelector("#number1").textContent = num1String;
+  document.querySelector("#operator").textContent = "";
+  document.querySelector("#number2").textContent = num2String;
+}
+document.querySelector("button#clear").addEventListener("click", clearDisplay);
+
+//TODO: Track place in array
+//Rules for array: last place in array must be a number
+//array[0] is number
+//array[1, 3, 5, 7, 9] must be operators
+
+//last place in array = array.length -1
 
 //Start with array with 1 empty string array[0]
 //when button is clicked, if it's an  operator button, do nothing
@@ -124,7 +129,7 @@ const numberKeys = document.querySelectorAll(".number");
 numberKeys.forEach((numberKey) =>
   numberKey.addEventListener("click", (e) => {
     if (operationDone) clearDisplay();
-
+    //FIXME:
     if (!isNum2) {
       num1String += e.target.id;
       updateDisplay(num1String, operation, num2String);
@@ -139,6 +144,8 @@ numberKeys.forEach((numberKey) =>
 //met)
 document.querySelectorAll(".row.operators, button.row#clear").forEach((btn) =>
   btn.addEventListener("click", () => {
+    //FIXME:
+
     //if num2String and op
     //Case when num1String is Active. If num1String isn't empty and operation are chosen, switch type mode to num2String
     if (!isNum2 && num1String.length > 0 && operation) {
